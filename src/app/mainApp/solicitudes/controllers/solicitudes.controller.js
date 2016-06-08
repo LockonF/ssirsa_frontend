778f57c789f293ac5c8d414c158f3ce81263f648
Olvera <luisenriho@gmail.com>
2016-06-08 07:46:43 -0500

    Solicitudes

--- src/app/mainApp/solicitudes/controllers/solicitudes.controller.js
+++ src/app/mainApp/solicitudes/controllers/solicitudes.controller.js
@@ -8,6 +8,74 @@
 
     function solicitudesController(){
         var vm = this;
+        vm.id=0;
+        vm.requisito = {
+            "id":vm.id,
+            "rTipo": null,
+            "rEstatus": null,
+            "rCantidad": null
+        };
+        vm.crearRequisito=crearRequisito;
+        vm.eliminarRequisito=eliminarRequisito;
+        vm.Requisitos = [];
+
+        // Crear requisito
+
+        function crearRequisito() {
+            console.log(vm.requisito)
+            if (vm.requisito != null) {
+                console.log("requisitos antes de agregarlo");
+                console.log(vm.Requisitos);
+                vm.id=vm.id+1;//ID
+                console.log("El id es:"+vm.id);
+                vm.Requisitos.id=vm.id;
+                vm.Requisitos.push(vm.requisito);
+                console.log("requisitos despues de agregarlo");
+                console.log(vm.Requisitos);
+
+                vm.requisito = {
+                    "id":vm.id,
+                    "rTipo": null,
+                    "rEstatus": null,
+                    "rCantidad": null
+                };
+
+                console.log("Los requisitos son:");
+                console.log(vm.Requisitos);
+            }
+        }
+
+        // Eliminar Requisito
+
+
+        function eliminarRequisito(requisito) {
+
+            vm.requisitocopy=requisito;
+            var index=0;
+
+            for (index = 0; index < vm.Requisitos.length; ++index) {
+
+                console.log("El requisito a borrar es:"+vm.requisitocopy.rTipo);
+                console.log(vm.Requisitos[index]);
+                if (vm.Requisitos[index].id == vm.requisitocopy.id) {
+
+                    console.log(index);
+                    //if(vm.Requisitos[index].Descripcion==vm.requisitocopy.Descripcion){
+                    console.log("voy a borrar");
+                    console.log(vm.Requisitos[index]);
+                    vm.Requisitos.splice(index, 1);
+                    //
+                }
+                else{console.log("Aun no lo encuentro")}
+
+            }
+
+        }
+
+        function editarRequisito(requisito) {
+
+
+        }
     }
 
 })();
--- src/app/mainApp/solicitudes/solicitudes.config.js
+++ src/app/mainApp/solicitudes/solicitudes.config.js
@@ -15,9 +15,15 @@
             controllerAs:'vm'
         })
             .state('triangular.admin-default.realizarSolicitud',{
-                url:'/crearSolicitudes',
+                url:'/realizarSolicitudes',
                 templateUrl:'app/mainApp/solicitudes/realizarSolicitud.tmpl.html',
-                controller:'solicitudesController',
+                controller:'realizarSolicitudController',
+                controllerAs:'vm'
+            })
+            .state('triangular.admin-default.Pruebas',{
+                url:'/Pruebas',
+                templateUrl:'app/mainApp/solicitudes/Pruebas.html',
+                controller:'Prueba',
                 controllerAs:'vm'
             })
 
@@ -35,6 +41,10 @@
             },{
                 name: 'Crear Solicitud',
                 state: 'triangular.admin-default.realizarSolicitud',
+                type: 'link'
+            },{
+                name: 'Pruebas',
+                state: 'triangular.admin-default.Pruebas',
                 type: 'link'
             }
 
