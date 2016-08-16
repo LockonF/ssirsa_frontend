e24990f074af85e28435ab86e47a194b71302646
Luis_Olvera <luisenriho@gmail.com>
2016-08-16 11:03:07 -0500

    alta de solicitudes completa. busqueda de solicitudes en desarrollo se agrego v-accordion se agrego angular-material-icons servicios de udn y tipo equipo solicitudes insertando y buscando, falta eliminar

--- src/app/mainApp/Admin/admin.config.js
+++ src/app/mainApp/Admin/admin.config.js
@@ -1,8 +1,9 @@
 /**
- * Created by franciscojaviercerdamartinez on 02/06/16.
+ * Created by franciscojaviercerdamartinez on 02/06/16 ffff.
  */
 (function () {
     'use strict';
+
     angular
         .module('app.mainApp.admin')
         .config(moduleConfig);
