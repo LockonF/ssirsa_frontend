1559479706a06730a30852f6487dc7ba1815c473
Luis_Olvera <luisenriho@gmail.com>
2016-08-30 08:59:31 -0500

    Gestion de usuario, upload foto busqueda de usuario solicitudes

--- src/app/app.constants.js
+++ src/app/app.constants.js
@@ -12,14 +12,14 @@
             //clientSecret:'CofY368VLw4iFkUtTuU5Rcle93NGCx93ec3FFajYdsb3dBPqZrGFsBqMaDqadD8f2oljocjP62LQaJkrZc8XKhcjuaxpUuMTGhJjKH6IfIATdkPBybsi1FxyKsBicYPX'
 
             //Staging Server
-            //URL:'http://45.55.251.215/',
-            //clientId:'pRKj87ZJd9AIupNbEdk3ZoEhcALnnTUgUPqhHngu',
-            //clientSecret:'r32EIetZwrUFxyzR10MYPZRt9sipdSFCkPDIVCLhZTgx8BhSO48KNlogyTzlSF1hmGRyA9XMl30JZvWgA2l8QqCjO8D3w4XxhT76SoZrtIDsPaigJDFjN9KhybjVu1zP'
+            URL:'http://45.55.251.215/',
+            clientId:'pRKj87ZJd9AIupNbEdk3ZoEhcALnnTUgUPqhHngu',
+            clientSecret:'r32EIetZwrUFxyzR10MYPZRt9sipdSFCkPDIVCLhZTgx8BhSO48KNlogyTzlSF1hmGRyA9XMl30JZvWgA2l8QqCjO8D3w4XxhT76SoZrtIDsPaigJDFjN9KhybjVu1zP'
 
             //local
-             URL:'http://127.0.0.1:8000/',
-             clientId:'6dBz0CYQAzsMSaGybUXWhTZ59uS3xA8geLnYejqb',
-             clientSecret:'cHIWLoDjfCINx2PscJdtU2SYOxrMD2hgdtHoKTZnaWtkUSDfXdV6CpIZC20fqGYgcZmr7i70tjwlpWsnkFfWfUH0YAU3cVdq7JcmHY9HLfKxQs0xRbktNXRAiOCKud0V'
+             //URL:'http://127.0.0.1:8000/',
+             //clientId:'6dBz0CYQAzsMSaGybUXWhTZ59uS3xA8geLnYejqb',
+             //clientSecret:'cHIWLoDjfCINx2PscJdtU2SYOxrMD2hgdtHoKTZnaWtkUSDfXdV6CpIZC20fqGYgcZmr7i70tjwlpWsnkFfWfUH0YAU3cVdq7JcmHY9HLfKxQs0xRbktNXRAiOCKud0V'
 
         })
 })();
