6b4fd8402d561cd8b4eb0585cf78aec119cc38c0
Christian Adan Israel <camezcua@mimoni.com>
2016-09-11 01:18:06 -0500

    Se hicieron cambios finales del calendario

--- src/app/mainApp/solicitudes/calendario/modal/editarSolicitud.dialog.controller.js
+++ src/app/mainApp/solicitudes/calendario/modal/editarSolicitud.dialog.controller.js
@@ -19,14 +19,14 @@
         vm.okClick = okClick;
         vm.selectedColor = null;
         // create start and end date of event
-        vm.atendida=moment();
+        //vm.atendida=moment();
         vm.start =moment(event.solicitud.fecha_inicio,"YYYY-MM-DD").toDate();
         if( event.solicitud.fecha_termino !== null) {
             vm.end = moment(event.solicitud.fecha_termino,"YYYY-MM-DD").toDate();
         }
-        if( event.solicitud.fecha_atendida !== null) {
+        /*if( event.solicitud.fecha_atendida !== null) {
             vm.atendida = moment(event.solicitud.fecha_atendida,"YYYY-MM-DD HH:mm:ss").toDate();
-        }
+        }*/
         vm.statu = [
             {
                 id:0,
@@ -56,12 +56,11 @@
             if(vm.event.solicitud.fecha_termino !== null) {
                 vm.event.end = updateEventDateTime(vm.end);
             }
-            if(vm.event.solicitud.fecha_atendida !== null) {
-                vm.event.atendida = updateEventDateTime(vm.atendida);
-            }
+
+            //vm.event.atendida = updateEventDateTime(vm.atendida);
             vm.event.solicitud.fecha_inicio=vm.event.start.format('YYYY-MM-DD');
             vm.event.solicitud.fecha_termino=vm.event.end.format('YYYY-MM-DD');
-            vm.event.solicitud.fecha_atendida=vm.event.atendida.format('"YYYY-MM-DD HH:mm:ss"');
+            //vm.event.solicitud.fecha_atendida=vm.event.atendida.format('"YYYY-MM-DD HH:mm:ss"');
             delete vm.event.solicitud.datos;
             Solicitudes_Admin.updateSolicitud(vm.event.solicitud).then(function (res) {
                 $mdDialog.hide(vm.event);
