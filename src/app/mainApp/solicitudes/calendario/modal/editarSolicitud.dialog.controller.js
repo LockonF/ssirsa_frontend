3c60234e84389495f33085a69e1bdb23acaf880a
Christian Adan Israel <camezcua@mimoni.com>
2016-09-10 21:10:59 -0500

    Se cambio ubicación de calendario dentro de solicitudes.

--- src/app/mainApp/solicitudes/calendario/modal/editarSolicitud.dialog.controller.js
+++ src/app/mainApp/solicitudes/calendario/modal/editarSolicitud.dialog.controller.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app.mainApp.calendar')
+        .module('app.mainApp.solicitudes')
         .controller('EditarSolicitudDialogController', EditarSolicitudDialogController);
 
     /* @ngInject */
--- src/app/mainApp/solicitudes/solicitudes.config.js
+++ src/app/mainApp/solicitudes/solicitudes.config.js
@@ -6,52 +6,40 @@
         .module('app.mainApp.solicitudes')
         .config(moduleConfig);
 
-    function moduleConfig($stateProvider, triMenuProvider){
+    function moduleConfig($stateProvider,$translatePartialLoaderProvider) {
+        $translatePartialLoaderProvider.addPart('app/mainApp/solicitudes');
         $stateProvider
-            .state('triangular.admin-default.solicitudes',{
-            url:'/solicitudes',
-            templateUrl:'app/mainApp/solicitudes/solicitud.tmpl.html',
-            controller:'solicitudesController',
-            controllerAs:'vm'
-        })
-            .state('triangular.admin-default.realizarSolicitud',{
-                url:'/realizarSolicitudes',
-                templateUrl:'app/mainApp/solicitudes/realizarSolicitud.tmpl.html',
-                controller:'realizarSolicitudController',
-                controllerAs:'vm'
+            .state('triangular.admin-default.solicitudes', {
+                url: '/solicitudes',
+                templateUrl: 'app/mainApp/solicitudes/solicitud.tmpl.html',
+                controller: 'solicitudesController',
+                controllerAs: 'vm'
             })
-            .state('triangular.admin-default.buscarSolicitud',{
-                url:'/buscarSolicitud',
-                templateUrl:'app/mainApp/solicitudes/buscarSolicitud.html',
-                controller:'buscarSolicitudController',
-                controllerAs:'vm'
+            .state('triangular.admin-default.realizarSolicitud', {
+                url: '/realizarSolicitudes',
+                templateUrl: 'app/mainApp/solicitudes/realizarSolicitud.tmpl.html',
+                controller: 'realizarSolicitudController',
+                controllerAs: 'vm'
             })
-            .state('triangular.admin-default.Pruebas',{
-                url:'/Pruebas',
-                templateUrl:'app/mainApp/solicitudes/Pruebas.html',
-                controller:'Prueba',
-                controllerAs:'vm'
+            .state('triangular.admin-default.buscarSolicitud', {
+                url: '/buscarSolicitud',
+                templateUrl: 'app/mainApp/solicitudes/buscarSolicitud.html',
+                controller: 'buscarSolicitudController',
+                controllerAs: 'vm'
             })
+            .state('triangular.admin-default.Pruebas', {
+                url: '/Pruebas',
+                templateUrl: 'app/mainApp/solicitudes/Pruebas.html',
+                controller: 'Prueba',
+                controllerAs: 'vm'
+            }).state('triangular.admin-default.calendar', {
+            url: '/calendar',
+            templateUrl: 'app/mainApp/solicitudes/calendar/calendar.tmpl.html',
+            controller: 'CalendarController',
+            controllerAs: 'vm'
+        });
 
-        /*triMenuProvider.addMenu({
-            name: 'Solicitudes',
-            icon: 'zmdi zmdi-bookmark',
-            type: 'dropdown',
-            priority: 4.1,
-            children: [{
-                name: 'Crear Solicitud',
-                state: 'triangular.admin-default.realizarSolicitud',
-                type: 'link'
-            },{
-                name: 'Buscar Solicitud',
-                state: 'triangular.admin-default.buscarSolicitud',
-                type: 'link'
-            }
-
-            ]
-        });*/
     }
-
 
 
 })();
