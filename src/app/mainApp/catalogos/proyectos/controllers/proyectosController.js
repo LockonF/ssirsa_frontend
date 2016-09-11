0a0ec954c2d0617765ca382318dcc48509f0bc9d
Emmanuel Plata <negrete93@gmail.com>
2016-09-11 12:01:19 -0500

    Feature catálogo de proyectos
    Commit inicial para creación de branch
    -Se borró la carpeta drectives del modulo entrada-salida ya que no será necesaria

---
+++ src/app/mainApp/catalogos/proyectos/controllers/proyectosController.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Emmanuel on 11/09/2016.
+ */
+(function(){
+    'use_strict';
+
+    angular
+        .module('app.mainApp.proyectos')
+        .controller('proyectosController',proyectosController);
+
+    function proyectosController(){
+        
+    }
+
+})();
