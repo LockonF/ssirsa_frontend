57e68d8ecaab1c1b5b035ee39899b9f2aad52e69
Luis_Olvera <luisenriho@gmail.com>
2016-09-15 22:21:22 -0500

    Correción de profile terminada

--- src/app/mainApp/services/persona.service.js
+++ src/app/mainApp/services/persona.service.js
@@ -21,26 +21,39 @@
             return Restangular.all('persona').customGET();
         }
 
-        function modify(object){
+        function modify(data){
 
 
-            /*var form_data = new FormData();
+            var form_data = new FormData();
 
+            form_data.append('id',data.id);
             form_data.append('nombre',data.nombre);
             form_data.append('apellido_paterno',data.apellido_paterno);
             form_data.append('apellido_materno',data.apellido_materno);
             form_data.append('direccion',data.direccion);
             form_data.append('telefono',data.telefono);
+            if(data.ife!=null)
             form_data.append('ife',data.ife);
+            if(data.foto!=null)
             form_data.append('foto',data.foto);
 
-            return Restangular.one('persona',object.id).withHttpConfig({transformRequest: angular.identity}).customPUT(form_data,"",{},{'Content-Type':undefined}).then(function(res){*/
-            return Restangular.one('persona',object.id).customPUT(object).then(function(resp){
+
+            var defer= $q.defer();
+            Restangular.one('persona',data.id).withHttpConfig({transformRequest: angular.identity}).customPUT(form_data,"",{},{'Content-Type':undefined}).then(function(res){
+                defer.resolve(res);
+                toastr.success('Entrada registrada correctamente','Éxito');
+            }).catch(function(err){
+                defer.resolve(err);
+                toastr.error('Error al registrar entrada', 'Error');
+                console.log(err);
+            });
+            //return Restangular.one('persona',form_data.id).withHttpConfig({transformRequest: angular.identity}).customPUT(form_data,"",{},{'Content-Type':undefined}).then(function(res){
+            /*return Restangular.one('persona',object.id).customPUT(object).then(function(resp){
                 console.log(resp);
                 return resp;
             }).catch(function(err){
                 console.log(err);
-            })
+            })*/
              //return baseURL.all(object.id).customPUT(object);
             //return object.put();
         }
