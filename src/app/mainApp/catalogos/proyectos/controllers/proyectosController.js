9ce6818f08e50e17c69daa05940c2fab4f1b2a75
Emmanuel Plata <negrete93@gmail.com>
2016-10-02 21:39:57 -0500

    Funcionalidad arreglada
    Falta verificar tamaño vertical de el elemento repeater

--- src/app/mainApp/catalogos/proyectos/controllers/proyectosController.js
+++ src/app/mainApp/catalogos/proyectos/controllers/proyectosController.js
@@ -18,27 +18,26 @@
         vm.remove = remove;
         vm.search = search;
         vm.clear = clear;
-        vm.selectedItemChange=selectedItemChange;
+        vm.clickCopy=clickCopy;
+        vm.querySearch=querySearch;
         
         activate();
-
+        vm.successTitle=Translate.translate('Projects.Notify.Success');
+        vm.errorTitle=Translate.translate('Projects.Notify.Error');
+        vm.warningTitle=Translate.translate('Projects.Notify.Warning');
+        vm.listErrorMessage=Translate.translate('Projects.Notify.Messages.ERROR_GETTING_PROJECTS');
+        vm.errorCreate=Translate.translate('Projects.Notify.Messages.ERROR_CREATING_PROJECT');
+        vm.succesCreate=Translate.translate('Projects.Notify.Messages.SUCCESS_CREATING_PROJECT');
+        vm.errorRemove=Translate.translate('Projects.Notify.Messages.ERROR_REMOVING_PROJECT');
+        vm.successRemove=Translate.translate('Projects.Notify.Messages.SUCCESS_REMOVING_PROJECT');
+        vm.errorUpdate=Translate.translate('Projects.Notify.Messages.ERROR_UPDATING_PROJECT');
+        vm.successUpdate=Translate.translate('Projects.Notify.Messages.SUCCESS_UPDATING_PROJECT');
+        vm.deleteButton=Translate.translate('MAIN.BUTTONS.DELETE');
+        vm.cancelButton=Translate.translate('MAIN.BUTTONS.CANCEL');
+        vm.dialogTitle=Translate.translate('MAIN.DIALOG.DELETE_TITLE');
+        vm.dialogMessage=Translate.translate('MAIN.DIALOG.DELETE_MESSAGE');
+        
         function activate() {
-            vm.successTitle=Translate.translate('Projects.Notify.Success');
-            vm.errorTitle=Translate.translate('Projects.Notify.Error');
-            vm.warningTitle=Translate.translate('Projects.Notify.Warning');
-            vm.listErrorMessage=Translate.translate('Projects.Notify.Messages.ERROR_GETTING_PROJECTS');
-            vm.errorCreate=Translate.translate('Projects.Notify.Messages.ERROR_CREATING_PROJECT');
-            vm.succesCreate=Translate.translate('Projects.Notify.Messages.SUCCESS_CREATING_PROJECT');
-            vm.errorRemove=Translate.translate('Projects.Notify.Messages.ERROR_REMOVING_PROJECT');
-            vm.successRemove=Translate.translate('Projects.Notify.Messages.SUCCESS_REMOVING_PROJECT');
-            vm.errorUpdate=Translate.translate('Projects.Notify.Messages.ERROR_UPDATING_PROJECT');
-            vm.successUpdate=Translate.translate('Projects.Notify.Messages.SUCCESS_UPDATING_PROJECT');
-            vm.deleteButton=Translate.translate('MAIN.BUTTONS.DELETE');
-            vm.cancelButton=Translate.translate('MAIN.BUTTONS.CANCEL');
-            vm.dialogTitle=Translate.translate('MAIN.DIALOG.DELETE_TITLE');
-            vm.dialogMessage=Translate.translate('MAIN.DIALOG.DELETE_MESSAGE');
-
-            vm.searchParameter='';
             vm.projects=Proyectos.list();
             vm.filteredProjects=vm.projects;
             vm.project=null;
@@ -94,21 +93,27 @@
             return vm.filteredProjects;
         }
 
+        function clickCopy(item) {
+            vm.selectedProject=item;
+            vm.project=angular.copy(item);
+            $scope.formProject.$invalid=true;
+        }
+
         function clear() {
             $scope.formProject.$setPristine();
             $scope.formProject.$setUntouched();
             $scope.formProject.$invalid=true;
             vm.searchParameter='';
             vm.filteredProjects=vm.projects;
+            vm.selectedProject=null;
             vm.project=null;
         }
 
-        function selectedItemChange(item) {
-            //vm.searchParameter='';
-            vm.project=angular.copy(item);
-            $scope.formProject.$invalid=true;
-        }
+        function querySearch(query) {
+            var results = query ? search(query) : vm.projects;
+            return results;
 
+        }
 
     }
 
