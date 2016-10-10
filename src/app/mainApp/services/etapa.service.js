c1615b08a17a35e515a9819b8b2e5a10aae80b9a
Christian Amezcua <amezcua9205@gmail.com>
2016-10-09 21:53:21 -0500

    Se agrego el Tipo de Equipo y Etapa en la vista de catalogo insumo, además de resolverse algunos bugs de la vista.

---
+++ src/app/mainApp/services/etapa.service.js
@@ -0,0 +1,36 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('Etapa', Etapa);
+
+    /* @ngInject */
+    function Etapa(Restangular) {
+        var baseUrl = Restangular.all('etapa');
+
+        return {
+            list:list,
+            update:update,
+            create:create,
+            remove:remove
+        };
+        function list(){
+            return baseUrl.getList();
+        }
+
+        function update(object)
+        {
+            return object.put();
+        }
+
+        function create(object){
+            return baseUrl.post(object);
+        }
+
+        function remove(object) {
+            return baseUrl.customDELETE(object.id,null,{'content-type':'application/json'});
+        }
+
+    }
+})();
