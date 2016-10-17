ac8f8b1653e087f2d45cf120c4c902dcf5a76f19
Emmanuel Plata <negrete93@gmail.com>
2016-10-16 20:51:01 -0500

    Añadidas funciones básicaas para el service de reportes

--- src/app/mainApp/services/reportes.service.js
+++ src/app/mainApp/services/reportes.service.js
@@ -10,6 +10,45 @@
 
     function Reportes(Restangular){
         var path= Restangular.all('report_builder').all('api');
-        
+        return {
+            getReports:getReports,
+            getReport:getReport,
+            getModels:getModels,
+            getRelatedModels:getRelatedModels,
+            getFields:getFields
+        };
+
+        //Obtiene todos los reportes existentes con su información básica
+        function getReports(){
+            return path.all("reports").getList().$object;
+        }
+        //Obtiene la información básica de un reporte en específico
+        function getReport(id){
+            return path.one("reports",id).customGET();
+        }
+        //Obtiene todos los modelos base sobre los cuales se pueden hacer reportes, filtros, obtener campos, etc.
+        function getModels(){
+            return path.all("contenttypes").getList();
+        }
+        //Obtiene los modelos relacionados con un modelo dado un ID de modelo
+        function getRelatedModels(id){
+            var request= {
+                "model": id,
+                "path": "",
+                "path_verbose": "",
+                "field": ""
+            };
+            return path.one("related_fields").post(request);
+        }
+        //Obtiene los campos de un modelo dado su ID
+        function getFields(id){
+            var request = {
+                "model": id,
+                "path": "",
+                "path_verbose": "",
+                "field": ""
+            };
+            return path.one("fields").post(request);
+        }
     }
 })();
