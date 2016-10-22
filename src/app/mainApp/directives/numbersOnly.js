ecd5ddb4abe5fd1fe5b096929a3a2b4f8465e4d3
Emmanuel Plata <negrete93@gmail.com>
2016-10-22 15:25:39 -0500

    Añadida nueva directiva para validacion de enteros, colocada en campo del dialog de cabinet para entrada manual

---
+++ src/app/mainApp/directives/numbersOnly.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Emmanuel on 22/10/2016.
+ */
+var app = angular.module('app.mainApp', []);
+
+app.directive('numbersOnly', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attr, ngModelCtrl) {
+            function fromUser(text) {
+                if (text) {
+                    var transformedInput = text.replace(/[^0-9]/g, '');
+
+                    if (transformedInput !== text) {
+                        ngModelCtrl.$setViewValue(transformedInput);
+                        ngModelCtrl.$render();
+                    }
+                    return transformedInput;
+                }
+                return undefined;
+            }
+            ngModelCtrl.$parsers.push(fromUser);
+        }
+    };
+});
