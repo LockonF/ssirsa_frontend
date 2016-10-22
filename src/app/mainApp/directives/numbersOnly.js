8fb0c020cc4658a02e7e87bbbdcecbe6cb928c41
Emmanuel Plata <negrete93@gmail.com>
2016-10-22 15:56:21 -0500

    Corregida directiva numbersOnly por falla en injector

--- src/app/mainApp/directives/numbersOnly.js
+++ src/app/mainApp/directives/numbersOnly.js
@@ -1,25 +1,29 @@
 /**
  * Created by Emmanuel on 22/10/2016.
  */
-var app = angular.module('app.mainApp', []);
+(function () {
+    'use strict';
+    angular
+        .module('app.mainApp')
+        .directive('numbersOnly',numbersOnly);
 
-app.directive('numbersOnly', function () {
-    return {
-        require: 'ngModel',
-        link: function (scope, element, attr, ngModelCtrl) {
-            function fromUser(text) {
-                if (text) {
-                    var transformedInput = text.replace(/[^0-9]/g, '');
-
-                    if (transformedInput !== text) {
-                        ngModelCtrl.$setViewValue(transformedInput);
-                        ngModelCtrl.$render();
+    function numbersOnly(){
+        return {
+            require: 'ngModel',
+            link: function (scope, element, attr, ngModelCtrl) {
+                function fromUser(text) {
+                    if (text) {
+                        var transformedInput = text.replace(/[^0-9]/g, '');
+                        if (transformedInput !== text) {
+                            ngModelCtrl.$setViewValue(transformedInput);
+                            ngModelCtrl.$render();
+                        }
+                        return transformedInput;
                     }
-                    return transformedInput;
+                    return undefined;
                 }
-                return undefined;
+                ngModelCtrl.$parsers.push(fromUser);
             }
-            ngModelCtrl.$parsers.push(fromUser);
-        }
-    };
-});
+        };
+    }
+})();
