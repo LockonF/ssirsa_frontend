5149238ebc0c87872656f5242baa541124acbabd
Emmanuel Plata <negrete93@gmail.com>
2016-10-27 19:47:19 -0500

    Terminada vista basica con validaciones segun tipo de insumo

--- src/app/mainApp/inventarios/insumo/insumo.controller.js
+++ src/app/mainApp/inventarios/insumo/insumo.controller.js
@@ -1,7 +1,7 @@
 /**
  * Created by franciscojaviercerdamartinez on 21/07/16.
  */
-(function(){
+(function () {
     'use strict';
 
     angular
@@ -11,26 +11,27 @@
     function insumoController(Translate, CatalogoInsumo, Insumo, toastr, Helper) {
         //Variable definition
         var vm = this;
-        vm.searchText="";
-        vm.showElements=false;
-        vm.choices=[
+        vm.searchText = "";
+        vm.cantidaad="1";
+        vm.showElements = false;
+        vm.choices = [
             Translate.translate('SUPPLIES.FIELDS.KIND_CHOICES.UNIQUE'),
             Translate.translate('SUPPLIES.FIELDS.KIND_CHOICES.LOT')
         ];
 
 
         //Translates
-        vm.successTitle=Translate.translate('MAIN.MSG.SUCCESS_TITLE');
-        vm.errorTitle=Translate.translate('MAIN.MSG.ERROR_TITLE');
-        vm.errorGenericMesssage=Translate.translate('MAIN.MSG.ERROR_MESSAGE');
-        
+        vm.successTitle = Translate.translate('MAIN.MSG.SUCCESS_TITLE');
+        vm.errorTitle = Translate.translate('MAIN.MSG.ERROR_TITLE');
+        vm.errorGenericMesssage = Translate.translate('MAIN.MSG.ERROR_MESSAGE');
+
         //Function parsing
-        vm.selectionChanged=selectionChanged;
-        vm.selectedItemChange=selectedItemChange;
-        vm.search=search;
+        vm.selectionChanged = selectionChanged;
+        vm.selectedItemChange = selectedItemChange;
+        vm.search = search;
 
         //Blank variables
-        var insumo= {
+        var insumo = {
             "cantidad": null,
             "no_remision": "",
             "usado": false,
@@ -43,42 +44,54 @@
         //Code
         activate();
 
-        function activate(){
-            CatalogoInsumo.listObject().then(function(res){
-                vm.catalogoInsumos= Helper.filterDeleted(res,true);                
-                vm.filteredInsumos={};
-                vm.uniqueInsumos= _.filter(vm.catalogoInsumos,function(element){
+        function activate() {
+            CatalogoInsumo.listObject().then(function (res) {
+                vm.catalogoInsumos = Helper.filterDeleted(res, true);
+                vm.filteredInsumos = {};
+                vm.uniqueInsumos = _.filter(vm.catalogoInsumos, function (element) {
                     return element.tipo === "U";
                 });
-                vm.lotInsumos= _.filter(vm.catalogoInsumos,function(element){
+                vm.lotInsumos = _.filter(vm.catalogoInsumos, function (element) {
                     return element.tipo === "L";
                 });
             });
-            vm.selectedInsumo=null;
+            vm.selectedInsumos = {};
+            vm.selectedInsumo = null;
             vm.selectedKind;
-            vm.isValid=false;
-            vm.insumo=angular.copy(insumo);
+            vm.isValid = false;
+            vm.insumo = angular.copy(insumo);
         }
 
-        function selectionChanged(){
-            if(vm.selectedKind == Translate.translate('SUPPLIES.FIELDS.KIND_CHOICES.UNIQUE'))
-                vm.filteredInsumos=vm.uniqueInsumos;
-            if(vm.selectedKind == Translate.translate('SUPPLIES.FIELDS.KIND_CHOICES.LOT'))
-                vm.filteredInsumos=vm.lotInsumos;
+        function selectionChanged() {
+            if (vm.selectedKind == Translate.translate('SUPPLIES.FIELDS.KIND_CHOICES.UNIQUE')) {
+                vm.selectedInsumos = vm.uniqueInsumos;
+                vm.filteredInsumos = vm.uniqueInsumos;
+            }
+
+            if (vm.selectedKind == Translate.translate('SUPPLIES.FIELDS.KIND_CHOICES.LOT')) {
+                vm.selectedInsumos = vm.lotInsumos;
+                vm.filteredInsumos = vm.lotInsumos;
+            }
         }
 
         function search(text) {
-            if(!angular.isUndefined(text)) {
-                    vm.filteredInsumos = _.filter(vm.catalogoInsumos, function (item) {
-                            return (item.palabra_clave.toLowerCase().startsWith(text.toLowerCase()) || item.descripcion.toLowerCase().includes(text.toLowerCase()));
-                    });
+            if (!angular.isUndefined(text)) {
+                vm.filteredInsumos = _.filter(vm.selectedInsumos, function (item) {
+                    return (item.palabra_clave.toLowerCase().startsWith(text.toLowerCase()) || item.descripcion.toLowerCase().includes(text.toLowerCase()));
+                });
                 vm.isValid = !((vm.filteredInsumos.length == 0 && text.length > 0) || (text.length > 0 && !angular.isObject(vm.selectedInsumo)));
                 return vm.filteredInsumos;
             }
         }
 
         function selectedItemChange(item) {
-            vm.isValid =angular.isObject(item);
+            vm.isValid = angular.isObject(item);
+        }
+
+        function clear(){
+            vm.cantidad="1";
+            vm.selectedInsumo=null;
+
         }
 
     }
