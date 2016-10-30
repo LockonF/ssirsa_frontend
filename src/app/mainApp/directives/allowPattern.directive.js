b266d7119fe5526241af707f8acbc8bdbd4262c1
Christian Adan Israel Amezcua Aguilar <amezcua9205@gmail.com>
2016-10-30 11:22:42 -0600

    Se agrego nueva directiva para permitir únicamente carácteres válidos para los campos de cabinet.

---
+++ src/app/mainApp/directives/allowPattern.directive.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Adan
+ */
+(function () {
+    'use strict';
+    angular
+        .module('app.mainApp')
+        .directive('allowPattern', allowPattern);
+
+    function allowPattern() {
+        return {
+            restrict: "A",
+            compile: function (tElement, tAttrs) {
+                return function (scope, element, attrs) {
+                    // I handle key events
+                    element.bind("keypress", function (event) {
+                        var keyCode = event.which || event.keyCode; // I safely get the keyCode pressed from the event.
+                        var keyCodeChar = String.fromCharCode(keyCode); // I determine the char from the keyCode.
+
+                        // If the keyCode char does not match the allowed Regex Pattern, then don't allow the input into the field.
+                        if (!keyCodeChar.match(new RegExp(attrs.allowPattern, "i"))) {
+                            event.preventDefault();
+                            return false;
+                        }
+
+                    });
+                };
+            }
+        };
+    }
+})();
