1c2db093e9431afc692cd8319fbe9825cc05c578
Emmanuel Plata <negrete93@gmail.com>
2016-10-30 21:26:28 -0600

    Modificados los services para la creación del reporte y previsualisación
    -Se modificó el service de getRelatedFields para permitir enviar
    los parámetros de path y field
    -Se modificó el service de getFields para permitir enviar
    los parámetros de path y field
    -Se añadió el service de generatePreview para permitir generar un reporte
    en formato JSON, util para previsualizar el reporte creado

--- src/app/mainApp/services/reportes.service.js
+++ src/app/mainApp/services/reportes.service.js
@@ -18,10 +18,12 @@
             getModels:getModels,
             getRelatedModels:getRelatedModels,
             getFields:getFields,
-            saveReport:saveReport
+            saveReport:saveReport,
+            createReport:createReport,
+            generatePreview:generatePreview
         };
 
-        //Obtiene todos los reportes existentes con su información básica
+        //Obtiene todos los reportes existentes con su información básica (util para listado en CRUD)
         function getPartialReports(){
             return path.all("reports").getList().$object;
         }
@@ -29,36 +31,80 @@
         function getPartialReport(id){
             return path.one("reports",id).customGET();
         }
-        //Obtiene todos los reportes existentes con su información completa (útil para guardar el reporte)
+        //Obtiene todos los reportes existentes con su información completa
         function getReports(){
             return path.all("report").all("").getList().$object;
         }
-        //Obtiene la información completa de un reporte en específico (útil para guardar un reporte)
+        //Obtiene la información completa de un reporte en específico (útil para ver, modificar y guardar un reporte)
         function getReport(id){
             return path.one("report",id).customGET();
         }
-        //Obtiene todos los modelos base sobre los cuales se pueden hacer reportes, filtros, obtener campos, etc.
+        //Obtiene todos los modelos base sobre los cuales se pueden hacer reportes, se usa solo al crearlo
         function getModels(){
             return path.all("contenttypes").getList();
         }
-        //Obtiene los modelos relacionados con un modelo dado un ID de modelo
-        function getRelatedModels(id){
-            var request= {
-                "model": id,
-                "path": "",
-                "path_verbose": "",
-                "field": ""
-            };
+        //Obtiene solamnte los modelos relacionados (campos que son llave primaria de otro modelo)
+        // con un modelo, dado un ID de modelo, y como opcion una ruta (path) y un nombre de campo (field)
+        // para mapear la ruta necesaria al crear el fieldset o filterset del reporte
+        function getRelatedModels(id,requestPath,field){
+            if(requestPath!==undefined) {
+                if(field!==undefined) {
+                    var request = {
+                        "model": id,
+                        "path": requestPath,
+                        "path_verbose": "",
+                        "field": field
+                    };
+                }
+                else{
+                    var request = {
+                        "model": id,
+                        "path": requestPath,
+                        "path_verbose": "",
+                        "field": ""
+                    };
+                }
+            }
+            else{
+                var request = {
+                    "model": id,
+                    "path": "",
+                    "path_verbose": "",
+                    "field": ""
+                };
+            }
             return path.one("related_fields").post(request);
         }
-        //Obtiene los campos de un modelo dado su ID
-        function getFields(id){
-            var request = {
-                "model": id,
-                "path": "",
-                "path_verbose": "",
-                "field": ""
-            };
+        //Obtiene todos los campos de un modelo, dado un ID de modelo, y como opcion una ruta (path)
+        // y un nombre de campo (field) para mapear la ruta necesaria al crear el fieldset
+        // o filterset del reporte
+        function getFields(id,requestPath,field){
+            if(requestPath!==undefined) {
+                if(field!==undefined) {
+                    var request = {
+                        "model": id,
+                        "path": requestPath,
+                        "path_verbose": "",
+                        "field": field
+                    };
+                }
+                else{
+                    var request = {
+                        "model": id,
+                        "path": requestPath,
+                        "path_verbose": "",
+                        "field": ""
+                    };
+                }
+            }
+            else{
+                var request = {
+                    "model": id,
+                    "path": "",
+                    "path_verbose": "",
+                    "field": ""
+                };
+            }
             return path.one("fields").post(request);
         }
         //Guardar un reporte (Incluso aunque sea nuevo, primero se crea vacio, luego se modifica
@@ -66,6 +112,14 @@
         //Utilizar como entrada un reporte obtenido mediante fullReport
         function saveReport(report){
             return path.all(report.id).customPUT(report);
+        }
+        //Permite crear un reporte nuevo con su nombre, descripción y modelo base
+        function createReport(data){
+            return path.all(report).post(data);
+        }
+
+        function generatePreview(id){
+            return path.one("report",id).all("generate").customGET();
         }
     }
 })();
