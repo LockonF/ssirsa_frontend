f9e967f5a7580d44f127d6b45068070c90e4e7cd
Emmanuel Plata <negrete93@gmail.com>
2016-11-09 13:47:58 -0600

    Creadas funciones de eliminar del listado, para los listados de campos y filtros

--- src/app/mainApp/reportes/edicion/reportEdicion.controller.js
+++ src/app/mainApp/reportes/edicion/reportEdicion.controller.js
@@ -1,31 +1,35 @@
 /**
  * Created by Emmanuel on 31/10/2016.
  */
-(function(){
+(function () {
     'use_strict';
 
     angular
         .module('app.mainApp.reportes')
-        .controller('reportEditionController',reportEditionController);
+        .controller('reportEditionController', reportEditionController);
 
-    function reportEditionController (Reportes, Translate, $stateParams, OPTIONS){
+    function reportEditionController(Reportes, Translate, $stateParams, OPTIONS) {
         //Variables
-        var vm=this;
+        var vm = this;
         vm.formato = "DD-MM-YYYY";
         vm.filterType = OPTIONS.filter;
         vm.days = OPTIONS.days;
 
+        //Function parse
+        vm.removeField = removeField;
+        vm.removeFilter = removeFilter;
+
         activate();
 
         //Translates
-        vm.tableDisplayHeaders=[
+        vm.tableDisplayHeaders = [
             Translate.translate('REPORTS.MODIFY.FIELD_NAME'),
             Translate.translate('REPORTS.MODIFY.FIELD_VERBOSE'),
             Translate.translate('REPORTS.MODIFY.FIELD_TYPE'),
             Translate.translate('REPORTS.MODIFY.DELETE')
         ];
 
-        vm.tableFilterHeaders=[
+        vm.tableFilterHeaders = [
             Translate.translate('REPORTS.MODIFY.FIELD_NAME'),
             Translate.translate('REPORTS.MODIFY.FIELD_VERBOSE'),
             Translate.translate('REPORTS.MODIFY.FIELD_TYPE'),
@@ -35,9 +39,22 @@
         //Blank templates
 
         //Functions
-        function activate(){
-            vm.report=Reportes.getReport($stateParams.id);
-            console.log(vm.report);
+        function activate() {
+            vm.report = Reportes.getReport($stateParams.id);
+            console.log(Reportes.getReport($stateParams.id));
         }
+
+        function removeField(id) {
+            vm.report.displayfield_set.splice(id, 1);
+        }
+
+        function removeFilter(id) {
+            var index = vm.report.filterfield_set.indexOf(id);
+            if (index > -1) {
+                vm.report.filterfield_set.splice(index, 1);
+            }
+        }
+
+        
     }
 })();
