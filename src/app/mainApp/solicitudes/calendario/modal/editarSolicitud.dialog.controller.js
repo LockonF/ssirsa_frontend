16f97ea9fd05a940ecd7e80095dc600c4f07b683
Christian Adan Israel Amezcua Aguilar <amezcua9205@gmail.com>
2016-11-30 00:48:39 -0600

    Se agrego una nueva directiva para la validación de las fechas dentro del calendario, actualmente solo esta habilitada para fecha de termino falta fecha de inicio.

--- src/app/mainApp/solicitudes/calendario/modal/editarSolicitud.dialog.controller.js
+++ src/app/mainApp/solicitudes/calendario/modal/editarSolicitud.dialog.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -6,7 +6,7 @@
         .controller('EditarSolicitudDialogController', EditarSolicitudDialogController);
 
     /* @ngInject */
-    function EditarSolicitudDialogController($mdDialog,OPTIONS, dialogData, event,Solicitudes_Admin) {
+    function EditarSolicitudDialogController($mdDialog, OPTIONS, dialogData, event, Solicitudes_Admin) {
 
         var vm = this;
         vm.cancelClick = cancelClick;
@@ -14,34 +14,33 @@
         vm.dialogData = dialogData;
         vm.event = event;
         vm.statu = OPTIONS.status;
-        vm.formato="DD-MM-YYYY";
-        var atendida={
-           fecha:moment().format('YYYY-MM-DD'),
-            hora:moment().format('HH:mm:ss')
+        vm.formato = "DD-MM-YYYY";
+        var atendida = {
+            fecha: moment().format('YYYY-MM-DD'),
+            hora: moment().toDate()
         };
         activate();
         function activate() {
-            vm.start =moment(event.solicitud.fecha_inicio).toDate();
-            if( event.solicitud.fecha_termino !== null) {
-                vm.end = moment(event.solicitud.fecha_termino).toDate();
-            }
-            vm.atendida=angular.copy(atendida);
-            if( event.solicitud.fecha_atendida !== null) {
-                vm.atendida.fecha=moment(event.solicitud.fecha_atendida,"YYYY-MM-DD").toDate();
-                vm.atendida.hora=moment(event.solicitud.fecha_atendida,"HH:mm:ss").toDate();
+            vm.start = event.solicitud.fecha_inicio;
+            vm.end = event.solicitud.fecha_termino;
+            vm.atendida = angular.copy(atendida);
+            if (event.solicitud.fecha_atendida !== null) {
+                vm.atendida.fecha = moment(event.solicitud.fecha_atendida, "YYYY-MM-DD").toDate();
+                vm.atendida.hora = moment(event.solicitud.fecha_atendida, "HH:mm:ss").toDate();
             }
         }
+
         function okClick() {
             vm.event.start = updateEventDateTime(vm.start);
-            if(vm.event.solicitud.fecha_termino !== null) {
+            if (vm.event.solicitud.fecha_termino !== null) {
                 vm.event.end = updateEventDateTime(vm.end);
             }
-            var fecha=updateEventDateTime(vm.atendida.fecha).subtract("day", 1);
-            var hora=updateEventDateTime(vm.atendida.hora);
-            fecha.set({ hour:hora.get('hour'), minute: hora.get('minute'), second: hora.get('second'), millisecond: hora.get('millisecond') });
-            vm.event.solicitud.fecha_inicio=vm.event.start.format('YYYY-MM-DD');
-            vm.event.solicitud.fecha_termino=vm.event.end.format('YYYY-MM-DD');
-            vm.event.solicitud.fecha_atendida=fecha.toISOString();
+            var fecha = updateEventDateTime(vm.atendida.fecha).subtract("day", 1);
+            var hora = updateEventDateTime(vm.atendida.hora);
+            fecha.set({hour: hora.get('hour'), minute: hora.get('minute'), second: hora.get('second'), millisecond: hora.get('millisecond')});
+            vm.event.solicitud.fecha_inicio = vm.event.start.format('YYYY-MM-DD');
+            vm.event.solicitud.fecha_termino = vm.event.end.format('YYYY-MM-DD');
+            vm.event.solicitud.fecha_atendida = fecha.toISOString();
             delete vm.event.solicitud.datos;
             Solicitudes_Admin.updateSolicitud(vm.event.solicitud).then(function (res) {
                 $mdDialog.hide(vm.event);
@@ -49,11 +48,13 @@
             });
 
         }
+
         function cancelClick() {
             $mdDialog.cancel();
         }
+
         function updateEventDateTime(date) {
-            return  moment(date);
+            return moment(date);
         }
     }
 })();
