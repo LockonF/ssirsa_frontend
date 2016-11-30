a62c47928216b4a989535debbf1225557145181c
Emmanuel Plata <negrete93@gmail.com>
2016-11-30 09:49:28 -0600

    Rama hotfix de tipo entrada

--- gulp/server.js
+++ gulp/server.js
@@ -32,7 +32,7 @@ function browserSyncInit(baseDir, files, browser) {
   });
 }
 
-gulp.task('serve', ['config','watch'], function () {
+gulp.task('serve', ['config:dev','watch'], function () {
   browserSyncInit([
     paths.tmp + '/serve',
     paths.src
