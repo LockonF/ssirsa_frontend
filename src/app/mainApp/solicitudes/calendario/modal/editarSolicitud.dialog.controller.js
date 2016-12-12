098d9e90e726d6f9c15959e6f5957cbc3e22a284
Christian Adan Israel Amezcua Aguilar <amezcua9205@gmail.com>
2016-12-11 22:43:28 -0600

    MODIFIED solicitudes/il18n/es.json: Se agregó mensaje de número máximno de solicitudes por día. MODIFIED editarSolicitud.dialog.controller.js: Se agregó mensaje de número máximno de solicitudes por día cuando se intenta confirmar más de 4 solicitudes, además de corregir un problema con la fecha de atención.

--- src/app/mainApp/solicitudes/calendario/modal/editarSolicitud.dialog.controller.js
+++ src/app/mainApp/solicitudes/calendario/modal/editarSolicitud.dialog.controller.js
@@ -6,7 +6,7 @@
         .controller('EditarSolicitudDialogController', EditarSolicitudDialogController);
 
     /* @ngInject */
-    function EditarSolicitudDialogController($mdDialog,toastr,Translate, CONFIGS, OPTIONS, dialogData, event, Solicitudes_Admin) {
+    function EditarSolicitudDialogController($mdDialog, toastr, Translate, CONFIGS, OPTIONS, dialogData, event, Solicitudes_Admin) {
 
         var vm = this;
         vm.cancelClick = cancelClick;
@@ -24,24 +24,29 @@
             vm.errorTitle = Translate.translate('MAIN.MSG.ERROR_TITLE');
             vm.errorMessage = Translate.translate('MAIN.MSG.ERROR_MESSAGE');
             vm.errorDate = Translate.translate('CALENDAR.FORM.MSG.FECHA_START');
+            vm.erroNumSolConf = Translate.translate('CALENDAR.FORM.MSG.ERRORNUMSOLCONF');
         }
 
         function okClick() {
             vm.event.start = moment(vm.start, 'DD/MM/YYYY');
             vm.event.end = moment(vm.end, 'DD/MM/YYYY');
-            var fecha = moment(vm.atendida, 'DD/MM/YYYY hh:mm').subtract(1, "day");
+            var fecha = moment(vm.atendida, 'DD/MM/YYYY hh:mm');
             vm.event.solicitud.fecha_inicio = vm.event.start.format('YYYY-MM-DD');
             vm.event.solicitud.fecha_termino = vm.event.end.format('YYYY-MM-DD');
             vm.event.solicitud.fecha_atendida = fecha.toISOString();
+            console.log(vm.event.solicitud);
             delete vm.event.solicitud.datos;
             Solicitudes_Admin.updateSolicitud(vm.event.solicitud).then(function (res) {
                 $mdDialog.hide(vm.event);
             }).catch(function (err) {
-                if(err.status==400 && err.data.fecha_inicio!=undefined)
-                {
-                     toastr.error(vm.errorDate,vm.errorTitle);
-                }else{
-                    toastr.error(vm.errorMessage,vm.errorTitle);
+                if (err.status == 400) {
+                    if (err.data.fecha_inicio != undefined) {
+                        toastr.error(vm.errorDate, vm.errorTitle);
+                    } else if (err.data.message == "Solo se pueden tener 4 solicitudes por día") {
+                        toastr.error(vm.erroNumSolConf, vm.errorTitle);
+                    }
+                } else {
+                    toastr.error(vm.errorMessage, vm.errorTitle);
                 }
             });
 
